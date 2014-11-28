40ca8815521b5f965965aa38faf0bea35b9f35d0
Hugo <hugo@Hugos-Mac.local>
2014-11-28 10:39:25 -0800

    Initial Commit

---
+++ Challenge2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Challenge2
+//
+//  Created by Hugo on 11/28/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
